959d825d8fd57e15b46097721a4eae212a1d1139
gme <agsproductionsnew@outlook.com>
2023-01-05 22:21:11 -0300

    Starting OpenGL implementation

---
+++ source/GcLib/opengl/OgLib.hpp
@@ -0,0 +1,24 @@
+//#ifndef __OPENGL_OGLIB__
+//#define __OPENGL_OGLIB__
+
+#include "OgConstant.hpp"
+
+#include "OgUtility.hpp"
+
+#include "OpenGLGraphics.hpp"
+#include "OgText.hpp"
+//#include "BlenderMesh.hpp"
+#include "RenderObject.hpp"
+#include "Shader.hpp"
+#include "Texture.hpp"
+
+#include "TransitionEffect.hpp"
+// #include"OgWindow.hpp"
+#include "OgScript.hpp"
+#include "ScriptManager.hpp"
+// #include"EventScript.hpp"
+
+//#include "OpenGLInput.hpp"
+//#include "OpenGLSound.hpp"
+
+#endif
