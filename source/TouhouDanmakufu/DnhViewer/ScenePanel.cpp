7ce64ce2f59a4e07952349420e588514d918750d
gme <agsproductionsnew@outlook.com>
2023-02-02 12:19:24 -0300

    Erasing the text in order to be able to fix the corruption (the previous commit just converted it to Shift-JIS)

--- source/TouhouDanmakufu/DnhViewer/ScenePanel.cpp
+++ source/TouhouDanmakufu/DnhViewer/ScenePanel.cpp
@@ -1,4 +1,4 @@
-#include "ScenePanel.hpp"
+ï»¿#include "ScenePanel.hpp"
 #include "MainWindow.hpp"
 
 /**********************************************************
@@ -36,7 +36,7 @@ bool ScenePanel::Initialize(HWND hParent)
 	bFixedArea_ = false;
 
 	buttonStart_.Create(hWnd_);
-	buttonStart_.SetText(L"ŠJn");
+	buttonStart_.SetText(L"");
 
 	panelPathEnemy_ = new ScriptPathPanel();
 	panelPathEnemy_->Initialize(ScriptPathPanel::TYPE_ENEMY, hWnd_);
@@ -46,7 +46,7 @@ bool ScenePanel::Initialize(HWND hParent)
 	WButton::Style styleCheck;
 	styleCheck.SetStyle(WS_CHILD | WS_VISIBLE | BS_PUSHLIKE | BS_AUTOCHECKBOX);
 	buttonPause_.Create(hWnd_, styleCheck);
-	buttonPause_.SetText(L"’â~");
+	buttonPause_.SetText(L"");
 	buttonPause_.SetWindowEnable(false);
 
 	GraphicsWindow* wndGraphics = MainWindow::GetInstance()->GetGraphicsWindow();
@@ -74,12 +74,12 @@ void ScenePanel::LocateParts()
 
 	if (!bFixedArea_) {
 /*
-		if (gWidth > gHeight * 4 / 3) { //‰¡•‚ªL‚¢
+		if (gWidth > gHeight * 4 / 3) { //
 			gWidth = gHeight * 4 / 3;
 			gHeight = gWidth * 3 / 4;
 			gHeight = gHeight > wHeight ? wHeight : gHeight;
 			gWidth = gHeight * 4 / 3;
-		} else { //c•‚ªL‚¢
+		} else { //Â
 			gHeight = gWidth * 3 / 4;
 			gHeight = gHeight > wHeight ? wHeight : gHeight;
 			gWidth = gHeight * 4 / 3;
@@ -122,7 +122,7 @@ bool ScenePanel::StartStg()
 	try {
 		ref_count_ptr<ScriptInformation> infoEnemy = panelPathEnemy_->GetSelectedScriptInformation();
 		if (infoEnemy == NULL)
-			throw gstd::wexception(L"“GƒXƒNƒŠƒvƒg‚ª•s³‚Å‚·");
+			throw gstd::wexception(L"");
 
 		ref_count_ptr<StgControllerForViewer> controller = StgControllerForViewer::Create();
 		if (infoEnemy->GetType() == ScriptInformation::TYPE_PACKAGE) {
@@ -133,13 +133,13 @@ bool ScenePanel::StartStg()
 		} else {
 			ref_count_ptr<ScriptInformation> infoPlayer = panelPathPlayer_->GetSelectedScriptInformation();
 			if (infoPlayer == NULL)
-				throw gstd::wexception(L"©‹@ƒXƒNƒŠƒvƒg‚ª•s³‚Å‚·");
+				throw gstd::wexception(L"");
 
 			ref_count_ptr<StgSystemInformation> infoStgSystem = new StgSystemInformation();
 			infoStgSystem->SetMainScriptInformation(infoEnemy);
 			controller->Initialize(infoStgSystem);
 
-			//ƒXƒe[ƒW
+			//
 			controller->Start(infoPlayer, NULL);
 		}
 
@@ -148,8 +148,8 @@ bool ScenePanel::StartStg()
 		ErrorDialog::ShowErrorDialog(e.what());
 		Logger::WriteTop(e.what());
 	} catch (...) {
-		MessageBox(hWnd_, L"ŠJn¸”s", L"error", MB_OK);
-		Logger::WriteTop(L"ŠJn¸”s");
+		MessageBox(hWnd_, L"", L"error", MB_OK);
+		Logger::WriteTop(L"");
 	}
 
 	return true;
@@ -163,13 +163,13 @@ bool ScenePanel::EndStg()
 void ScenePanel::SetStgState(bool bStart)
 {
 	if (bStart) {
-		buttonStart_.SetText(L"I—¹");
+		buttonStart_.SetText(L"Â");
 		buttonPause_.SetWindowEnable(true);
 
 		panelPathEnemy_->SetWindowEnable(false);
 		panelPathPlayer_->SetWindowEnable(false);
 	} else {
-		buttonStart_.SetText(L"ŠJn");
+		buttonStart_.SetText(L"");
 		buttonPause_.SetWindowEnable(false);
 		panelPathEnemy_->SetWindowEnable(true);
 		panelPathPlayer_->SetWindowEnable(true);
@@ -211,14 +211,14 @@ bool ScenePanel::ScriptPathPanel::Initialize(int type, HWND hParent)
 	{
 		labelPath_.Create(hWnd_);
 		if (type == TYPE_ENEMY)
-			labelPath_.SetText(L"“GƒXƒNƒŠƒvƒg");
+			labelPath_.SetText(L"");
 		else
-			labelPath_.SetText(L"©‹@ƒXƒNƒŠƒvƒg");
+			labelPath_.SetText(L"");
 
 		editPath_.Create(hWnd_, styleEdit);
 
 		buttonPath_.Create(hWnd_, stylePath);
-		buttonPath_.SetText(L"‘I‘ğ");
+		buttonPath_.SetText(L"");
 	}
 
 	labelPath_.SetBounds(8, 12, 72, 20);
@@ -259,8 +259,8 @@ LRESULT ScenePanel::ScriptPathPanel::_WindowProcedure(HWND hWnd, UINT uMsg, WPAR
 				ofn.Flags = OFN_EXPLORER | OFN_HIDEREADONLY;
 				ofn.nFilterIndex = 1;
 				ofn.lpstrDefExt = ".txt";
-				ofn.lpstrFilter = "‘S‚Ä‚Ìƒtƒ@ƒCƒ‹";
-				ofn.lpstrTitle = "ƒXƒNƒŠƒvƒg‚ğŠJ‚­";
+				ofn.lpstrFilter = "";
+				ofn.lpstrTitle = "Â­";
 				if (GetOpenFileName(&ofn))
 					editPath_.SetText(path);
 */
