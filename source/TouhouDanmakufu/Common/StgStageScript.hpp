6de57ecce591847093513fb0795f3aea0e6593fb
gme <agsproductionsnew@outlook.com>
2023-02-12 01:20:49 -0300

    Added more comenting for the 0.12m functions in StgStageScript.hpp

--- source/TouhouDanmakufu/Common/StgStageScript.hpp
+++ source/TouhouDanmakufu/Common/StgStageScript.hpp
@@ -377,14 +377,14 @@ public:
 
 
 	//0.12m
-
+	//移動系関数/Movement-type Functions
 	static gstd::value Func_Orig_SetMovePositionHermite(gstd::script_machine* machine, int argc, gstd::value const* argv);
 	static gstd::value Func_Orig_SetMovePositionRandom01(gstd::script_machine* machine, int argc, gstd::value const* argv);
 	static gstd::value Func_Orig_SetPlayerX(gstd::script_machine* machine, int argc, gstd::value const* argv);
 	static gstd::value Func_Orig_SetPlayerY(gstd::script_machine* machine, int argc, gstd::value const* argv);
 	static gstd::value Func_Orig_SetCoordinateType(gstd::script_machine* machine, int argc, gstd::value const* argv);
 	static gstd::value Func_Orig_SetOrigin(gstd::script_machine* machine, int argc, gstd::value const* argv);
-
+	//敵弾を制御する関数/Bullet-Controlling Functions
 	static gstd::value Func_Orig_SetShotDirectionType(gstd::script_machine* machine, int argc, gstd::value const* argv);
 	static gstd::value Func_Orig_CreateShotA(gstd::script_machine* machine, int argc, gstd::value const* argv);
 	static gstd::value Func_Orig_CreateLaserA(gstd::script_machine* machine, int argc, gstd::value const* argv);
