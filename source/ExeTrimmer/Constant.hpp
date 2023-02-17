55264beeda78658bc13fec2f8caabb269a197dff
gme <agsproductionsnew@outlook.com>
2023-02-17 16:08:54 -0300

    Added ExeTrimmer's code

--- source/ExeTrimmer/Constant.hpp
+++ source/ExeTrimmer/Constant.hpp
@@ -1,1 +1,16 @@
-#pragma once
+#ifndef __CONSTANT__
+#define __CONSTANT__
+
+#include "../GcLib/gstd/GstdLib.hpp"
+
+#include "resource.h"
+
+#include <set>
+
+using namespace gstd;
+
+const std::wstring DNH_VERSION = L" 0.6.2";
+const std::wstring WINDOW_TITLE = L"EXE Trimmer-V";
+const std::wstring PATH_ENVIRONMENT = L"env.dat";
+
+#endif
---
+++ source/ExeTrimmer/LibImpl.cpp
@@ -0,0 +1,1 @@
+#include "LibImpl.hpp"
