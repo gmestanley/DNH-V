8e6a62484feece39ba7792ff8ad37c790ac1230d
gme <agsproductionsnew@outlook.com>
2023-02-27 11:53:49 -0300

    Successfully remade ExeTrimmer's resource

---
+++ source/ExeTrimmer/resource.h
@@ -0,0 +1,26 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by ExeTrimmer.rc
+#define IDD_DIALOG_MAIN                 101
+#define IDR_MENU_MAIN                   102
+#define IDI_ICON                        103
+#define IDC_EDIT_OPTION                 1000
+#define IDC_LIST_FILE                   1001
+#define IDC_BUTTON_ADD                  1002
+#define IDC_BUTTON_DELETE               1003
+#define IDC_BUTTON_DECOMPILE            1004
+#define IDC_BUTTON_ARCHIVE              1004
+#define ID_MENUITEM_EXIT                40001
+#define ID_MENUITEM_ADD                 40002
+#define ID_MENUITEM_VERSION             40003
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
