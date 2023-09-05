ec438ba2c40befac1f72000c6ffabebad207a33e
gme <agsproductionsnew@outlook.com>
2023-09-05 11:54:29 -0300

    DirectX files spontaneously added (no clue why)

---
+++ source/GcLib/directx/DxLib.hpp
@@ -0,0 +1,25 @@
+#ifndef __DIRECTX_DXLIB__
+#define __DIRECTX_DXLIB__
+
+#include "DxConstant.hpp"
+
+#include "DxUtility.hpp"
+
+#include "DirectGraphics.hpp"
+#include "DxText.hpp"
+#include "ElfreinaMesh.hpp"
+#include "MetasequoiaMesh.hpp"
+#include "RenderObject.hpp"
+#include "Shader.hpp"
+#include "Texture.hpp"
+
+#include "TransitionEffect.hpp"
+// #include"DxWindow.hpp"
+#include "DxScript.hpp"
+#include "ScriptManager.hpp"
+// #include"EventScript.hpp"
+
+#include "DirectInput.hpp"
+#include "DirectSound.hpp"
+
+#endif
